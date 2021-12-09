db2b140850fe411cb5d161524ca3c0b7880f2ffe
Bodi <bodiaugustus@outlook.com>
2021-12-08 20:09:01 -0600

    Set up React router in App.js using imports from the pages folder index.js file which was also setup.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,37 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
+import {Home, About, SingleProducts, Error, Cart, Checkout, Products} from './pages';
+
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+    <Navbar/>
+    <Sidebar/>
+      <Switch>
+        <Route path="/" exact>
+          <Home/>
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route path="/cart">
+          <Cart/>
+        </Route>
+        <Route path="/products">
+          <Products/>
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProducts/>}/>
+        <Route path="/checkout">
+          <Checkout/>
+        </Route>
+        <Route path="*">
+          <Error/>
+        </Route>
+      </Switch>
+      <Footer/>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './HomePage';
+import Products from './ProductsPage';
+import SingleProducts from './SingleProductPage';
+import About from './AboutPage';
+import Cart from './CartPage';
+import Error from './ErrorPage';
+import Checkout from './CheckoutPage';
+import PrivateRoute from './PrivateRoute';
+
+export {Home,PrivateRoute,Products,SingleProducts,About,Cart,Checkout,Error}
