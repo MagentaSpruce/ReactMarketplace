0ebb8828ad1bf7757dbc08f226e3ac0e39b46d5e
Bodi <bodiaugustus@outlook.com>
2021-12-09 21:13:19 -0600

    Set up the Services component.

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,30 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <article className="header">
+          <h3>custom furniture <br /> built only for you</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptate magnam alias deleniti omnis perspiciatis inventore nulla, ratione consequatur quaerat saepe voluptates.</p>
+        </article>
+        <div className="services-center">
+          {services.map((service) => {
+            const {id, icon, title, text} = service;
+            return (
+              <article className="service" key={id}>
+                <span className="icon">
+                  {icon}
+                </span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
