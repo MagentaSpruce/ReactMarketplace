e1b45535e643fa18900796d3ea2925ca973477cf
Bodi <bodiaugustus@outlook.com>
2021-12-11 21:00:49 -0600

    Set up return for SingleProduct component.

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -14,58 +14,62 @@ import {
 } from '../actions'
 
 const initialState = {
-  isSidebarOpen:false,
+  isSidebarOpen: false,
   products_loading: false,
   products_error: false,
   products: [],
   featured_products: [],
-  single_product_loading:false,
-  single_product_error:false,
-  single_product:{},
-
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 }
 
 const ProductsContext = React.createContext()
 
 export const ProductsProvider = ({ children }) => {
-  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   const openSidebar = () => {
-    dispatch({type: 'SIDEBAR_OPEN'})
+    dispatch({ type: SIDEBAR_OPEN })
   }
-
   const closeSidebar = () => {
-    dispatch({type: 'SIDEBAR_CLOSE'})
+    dispatch({ type: SIDEBAR_CLOSE })
   }
 
   const fetchProducts = async (url) => {
-    // const resp = await axios.get(url)
-    dispatch({type: GET_PRODUCTS_BEGIN});
+    dispatch({ type: GET_PRODUCTS_BEGIN })
     try {
-      const resp = await fetch(url);
-      const products = await resp.json();
-      dispatch({type: GET_PRODUCTS_SUCCESS, payload: products});
+      const response = await axios.get(url)
+      const products = response.data
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
     } catch (error) {
-      dispatch({type: GET_PRODUCTS_ERROR})
+      dispatch({ type: GET_PRODUCTS_ERROR })
     }
   }
-
   const fetchSingleProduct = async (url) => {
-    dispatch({type: GET_SINGLE_PRODUCT_BEGIN});
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN })
     try {
-      const response = await axios(url);
-      const singleProduct = await require(response);
-      dispatch({type: GET_PRODUCTS_SUCCESS, payload: singleProduct})
+      const response = await axios.get(url)
+      const singleProduct = response.data
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct })
     } catch (error) {
-      dispatch({type: GET_PRODUCTS_ERROR});
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR })
     }
   }
 
   useEffect(() => {
-    fetchProducts(url);
-  },[])
+    fetchProducts(url)
+  }, [])
+
   return (
-    <ProductsContext.Provider value={{...state, openSidebar,closeSidebar, fetchSingleProduct}}>
+    <ProductsContext.Provider
+      value={{
+        ...state,
+        openSidebar,
+        closeSidebar,
+        fetchSingleProduct,
+      }}
+    >
       {children}
     </ProductsContext.Provider>
   )
