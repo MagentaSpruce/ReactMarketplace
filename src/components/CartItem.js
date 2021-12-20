5db92af6dabfb76499841b9c1cde82ac73d54013
Bodi <bodiaugustus@outlook.com>
2021-12-20 14:30:30 -0600

    Set up the cart totals component.

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -4,8 +4,17 @@ import { formatPrice } from '../utils/helpers'
 import AmountButtons from './AmountButtons'
 import { FaTrash } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
-const CartItem = () => {
-  return <h4>cart item</h4>
+const CartItem = ({id, image, name, color, price, amount}) => {
+  const {removeItem, toggleAmount} = useCartContext()
+  const increase = () => {
+
+  }
+  const decrease = () => {}
+  return <Wrapper>
+    <div className="title">
+
+    </div>
+  </Wrapper>
 }
 
 const Wrapper = styled.article`
