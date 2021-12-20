b70bc10111cc99ea0c585dce73424eaebf064ca7
Bodi <bodiaugustus@outlook.com>
2021-12-20 11:10:45 -0600

    Started setting up functionality inside of cartReducer. Set up newItem.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import { Auth0Provider } from '@auth0/auth0-react'
 ReactDOM.render(
     <ProductsProvider>
         <FilterProvider>
+        <CartProvider>
             <App />
+        </CartProvider>
         </FilterProvider>
     </ProductsProvider>, document.getElementById('root'))
