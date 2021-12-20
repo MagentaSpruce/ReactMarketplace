3f03bbbd466de50ff1d2fe70fe7d17cc61879297
Bodi <bodiaugustus@outlook.com>
2021-12-20 14:37:23 -0600

    Finished setting up cart page UI.

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -12,8 +12,21 @@ const CartItem = ({id, image, name, color, price, amount}) => {
   const decrease = () => {}
   return <Wrapper>
     <div className="title">
-
+      <img src={image} alt={name} />
+      <div>
+        <h5 className="name">{name}</h5>
+        <p className="color">
+          color : <span style={{background:color}}></span>
+        </p>
+        <h5 className="price-small">{formatPrice(price)}</h5>
+      </div>
     </div>
+    <h5 className="price">{formatPrice(price)}</h5>
+    <AmountButtons amount={amount} increase={increase} decrease={decrease}/>
+    <h5 className="subtotal">
+      {formatPrice(price * amount)}
+    </h5>
+    <button type='button' className='remove-btn' onClick={()=>removeItem(id)}><FaTrash/></button>
   </Wrapper>
 }
 
